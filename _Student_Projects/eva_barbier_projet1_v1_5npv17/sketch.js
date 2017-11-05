2d5c075e980382531d213ea84a1fcff5d49bab04
jguess <lists@guess.fr>
2017-11-05 23:03:33 +0100

    eva barbier

---
+++ _Student_Projects/eva_barbier_projet1_v1_5npv17/sketch.js
@@ -0,0 +1,99 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255, 200, 200);
+}
+
+function draw(){}
+
+function soundPlayer() {
+  mySound.play();
+}
+
+var mySound;
+var counter = 0;
+var soundList = ['CFT_Lite_Plucked_Arp_Em_85_BPM.wav', 'CFT_Rim_Click_Fill.wav', 'CFT_Sub_Kick_A#m.wav'];
+
+
+function keyPressed() {
+
+  if ((key == 'a') || (key == 'A')) {
+    background(77, 24, 49);
+    stroke(255);
+    strokeWeight(5);
+    line(random(width), random(height), random(width), random(height));
+  }
+
+
+  if ((key == 'z') || (key == 'Z')) {
+    background(252, 255, 30);
+    strokeWeight(0);
+    fill(random(255), random(255), random(255));
+    triangle(random(width), random(height), random(width), random(height), random(width), random(height));
+  }
+
+
+if ((key == 'e') || (key == 'E')) {
+    background(30, 180, 255);
+    ellipse(random(width), random(height), 105, 105);
+    fill(random(255), random(255), random(255));
+  }
+
+
+if ((key == 'r') || (key == 'R')) {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(166, 30, 255);
+  rotateX(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  cone(200, 200);
+}
+
+
+if ((key == 't') || (key == 'T')) {
+  background(255, 78, 30);
+  if (millis() > 1000) {
+    fill(random(255), random(255), random(255));
+    ellipse(random(width), random(height), random(50, 100), random(50, 100));
+}
+}
+
+
+if ((key == 'y') || (key == 'Y')) {
+  background(255, 30, 136);
+  fill(random(255), random(255), random(255));
+  rect(140, 200, 105, 360);
+  rect(380, 200, 105, 250);
+}
+
+}
+
+function keyReleased() {
+  background(255, 200, 200);
+}
+
+
+
+ if ((key == 'a') || (key == 'A')) {
+  loadSound('sounds/CFT_Lite_Plucked_Arp_Em_85_BPM.wav' + soundList[counter], ready);
+  counter = counter + 1;
+  if (counter >= soundList.length) {
+    counter = 0;
+  }
+}
+function ready(sound) {
+  mySound = sound;
+  mySound.play();
+ }
+
+
+ if ((key == 'z') || (key == 'Z')) {
+  loadSound('CFT_Rim_Click_Fill.wav' + soundList[counter], ready);
+  counter = counter + 1;
+  if (counter >= soundList.length) {
+    counter = 0;
+  }
+}
+function ready(sound) {
+  mySound = sound;
+  mySound.play();
+
+ }
