0404af3d68f39a3caa9331a02cc1d7e3cd419c65
jguess <lists@guess.fr>
2017-11-17 14:18:24 +0100

    melusine v1

---
+++ _Student_Projects/mélusine_wagner_projet1_v1_12nov17/sketch.js
@@ -0,0 +1,30 @@
+
+var x;
+var img
+var images
+var zoom = 1
+
+function setup() {
+	imageMode(CENTER);
+	image (img, width*2, height*2);
+	scale(zoom);
+
+	images = ["imgs/capture11.PNG, imgs/Capture12.PNG, imgs/Capture13.PNG, imgs/Capture21.PNG, imgs/Capture25.PNG, imgs/Capture26.PNG, imgs/Capture27.PNG,  imgs/Capture28.PNG"];
+	img = loadImage(random(images));
+	//affiche une img random a chq chargement
+}
+
+function mousePressed () {
+	
+	img = loadImage(random(images));
+	//affiche une nouvelle image a chq click ?
+}
+
+function mouseWheel(event) {
+
+  zoom += sensativity * event.delta;
+  
+
+//zoomer sur l'image suivant la position mouseX,mouseY ?
+
+	
