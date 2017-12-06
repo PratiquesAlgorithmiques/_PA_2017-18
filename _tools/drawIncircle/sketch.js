5edc297e81c62989e019a13c88d364ca6b1fee3e
Alx <alngo@student.42.fr>
2017-12-06 18:57:21 +0100

    amelioration function

--- _tools/drawIncircle/sketch.js
+++ _tools/drawIncircle/sketch.js
@@ -6,8 +6,8 @@ function setup() {
 }
 
 function draw() {
-	i = i > 360 ? 0 : i;
-	background(255);
+	i = (i > 360) ? 0 : i;		// syntaxe ternaire : "i = [condition] ? [oui] : [non];"
+	background(255);		// page en blanc
 	fill(255, 0, 0);		// remplir en rouge
 	ellipse(200, 200, 10);		// dessiner une ellipse pour l'origine
 	fill(0, 255, 0);		// remplir en vert
@@ -20,18 +20,21 @@ function drawInCircle(x, y, rayon, degree) {
 	// "rayon" permet de decaler la position en pixel. (ici, decalage sur l'axe y).
 	// "degree" est l'angle voulu par rapport a l'origine.
 
+	// Les deux machins qui suivent constituent le coeur de la rotation
 	// Conversion degree en radian, pour cosinus et sinus
 	var radian = degree * (PI / 180);
-	
 	// Calcul de la nouvelle coordonee :
 	// Application d'une rotation et d'une translation.
-	this.x = x + (0 * Math.cos(radian) - rayon * Math.sin(radian));
-	this.y = y + (0 * Math.sin(radian) + rayon * Math.cos(radian));
+	this.mcos = cos(radian);
+	this.msin = sin(radian);
+	this.x = x + (0 * this.mcos - rayon * this.msin);
+	this.y = y + (0 * this.msin + rayon * this.mcos);
 
 	// Le machin a dessiner selon le this.x et this.y
 	// Vous pouvez modifier comme bon vous semble :)
-	// pour mettre des trais etc.. faire une horloge...
+	// pour mettre des lignes etc...
 	// ==============================================
 	ellipse(this.x, this.y, 50);
+	line(x, y, this.x, this.y);
 	// ==============================================
 }
